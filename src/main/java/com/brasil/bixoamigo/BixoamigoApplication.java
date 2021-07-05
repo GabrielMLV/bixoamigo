a745d98de9c621314c5858951c4c64b7976f5fe6
Gabriel Matheus <bielmatheus20@gmail.com>
2021-07-05 00:34:28 -0300

    primeiro commit

---
+++ src/main/java/com/brasil/bixoamigo/BixoamigoApplication.java
@@ -0,0 +1,13 @@
+package com.brasil.bixoamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BixoamigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BixoamigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brasil/bixoamigo/BixoamigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasil.bixoamigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BixoamigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
